4ccf7f4f774981fc67bb51d8a13d7893f847d70c
Oseib4 <oseibn4@gmail.com>
2023-10-28 18:25:19 +0200

    modified

--- book-app/src/componets/Footer.jsx
+++ book-app/src/componets/Footer.jsx
@@ -7,18 +7,16 @@ const myFooter = () => {
 
 
 return (
-    <footer className="text-center text-white fixed-bottom" style={{ backgroundColor: '#21081a' }}>
-      {/* Grid container */}
-      <div className="container p-4"></div>
-      {/* Grid container */}
+    <myFooter className="text-center text-white fixed-bottom" style={{ backgroundColor: '#21081a' }}>
       
-      {/* Copyright */}
+      <div className="container p-4"></div>
+     
       <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
         © 2020 Copyright:
         <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
       </div>
-      {/* Copyright */}
-    </footer>
+      </myFooter>
+    
   );
 
 }
--- book-app/src/componets/Navigation.jsx
+++ book-app/src/componets/Navigation.jsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
 
 
 const Navigation = () => {
     const [mynavigation, setMynavigation] = useState([]);
+
+
+
+
+    <> 
+     <Navbar
+    style={{ width: "100%", margin: "0 auto", height: "100px" }}
+    fixed="top"
+    expand="lg"
+    className="bg-body-tertiary"
+  >
+      <Container>
+        <Navbar.Brand href="home id"></Navbar.Brand>
+</Container>
+
+  </Navbar>
+    </>
 }
 
 
